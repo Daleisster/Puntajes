3f342005cfc95e021644ff97e65c8f1fa8e60893
Daleisster <damihhuerta@gmail.com>
2026-06-24 03:42:17 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    set,
+    get
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBvEXT5k4kvaqq3jW394Fq5ueom4CxMF2o",
+    authDomain: "puntajes-kutral.firebaseapp.com",
+    databaseURL: "https://puntajes-kutral-default-rtdb.firebaseio.com",
+    projectId: "puntajes-kutral",
+    storageBucket: "puntajes-kutral.firebasestorage.app",
+    messagingSenderId: "1048990383919",
+    appId: "1:1048990383919:web:18ccbea65e0b16435febda"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+export { ref, set, get };
