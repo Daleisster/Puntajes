72a1808def4cf526b67f56c6f2b71940b7005050
Daleisster <damihhuerta@gmail.com>
2026-06-25 03:18:00 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import {
-    getDatabase,
-    ref,
-    set,
-    get
-} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js"
+import {getDatabase, ref, set, get, onValue} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js"
 const firebaseConfig = {
     apiKey: "AIzaSyBvEXT5k4kvaqq3jW394Fq5ueom4CxMF2o",
     authDomain: "puntajes-kutral.firebaseapp.com",
@@ -15,8 +10,6 @@ const firebaseConfig = {
     messagingSenderId: "1048990383919",
     appId: "1:1048990383919:web:18ccbea65e0b16435febda"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getDatabase(app);
-export { ref, set, get };
+const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
+export { ref, set, get, onValue}
