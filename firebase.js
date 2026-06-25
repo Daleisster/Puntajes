55e460e6f60bf94466c5657185e3e2959674bbce
Daleisster <damihhuerta@gmail.com>
2026-06-25 03:25:20 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js"
 import {getDatabase, ref, set, get, onValue} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js"
 const firebaseConfig = {
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 export const db = getDatabase(app)
 export { ref, set, get, onValue}
+
